b8d0d2a6d4a65c5115063ec88d4e40a75d0ed90f
unruffled-happiness <andersonk@mbc.qld.edu.au>
2020-11-19 12:46:34 +1000

    Assingment

--- Assingment term 4.py
+++ Assingment term 4.py
@@ -2,7 +2,7 @@ from sense_hat import SenseHat
 import random
 import time
 sense = SenseHat()
-
+ 
 #All setup 
 
 #define colours
@@ -73,85 +73,95 @@ right_arrow = [
 arrow = clear
 
 
-
+sense.show_message("click to start")
 sense.set_pixels(clear)
 #Set random numbers (random.randit(1,4))
-
-#Setup ends here
-anum = 1 
-ana = 0
-clock = 0
-for num in range(1,11):
-    anum =(random.randint(1,4))
-    pause =(random.randint(10,40)/15)
-    if anum == 1:
-        arrow = up_arrow
-    elif anum == 2:
-        arrow = down_arrow
-    elif anum == 3:
-        arrow = left_arrow
-    else:
-        anum == right_arrow
-    sense.set_pixels(arrow)
-    start_time = time.time()
-    time.sleep(pause)
-    
-    #Checking for movment in the joystick
-    while ana != anum:
+# Making it loop infinately
+while True:
+    run = 0
+    while run == 0:
         for event in sense.stick.get_events():
-       # Check if joystick is pressed
             if event.action == 'pressed':
+                run = 1
+
+
+
+    while run == 1:
+    #Setup ends here
+        anum = 1 
+        ana = 0
+        clock = 0
+        trys = 0
+        average = 0 
+        #for num in range(1,11):
+        while trys != 9:
+            if clock == 0:
+                trys = trys - 1 
+            anum =(random.randint(1,4))
+            pause =(random.randint(10,40)/15)
+            if anum == 1:
+                arrow = up_arrow
+            elif anum == 2:
+                arrow = right_arrow
+            elif anum == 3:
+                arrow = down_arrow
+            elif anum == 4:
+                arrow = left_arrow
+            sense.set_pixels(arrow)
+            start_time = time.time()
             
-            # Check which direcion has been pressed
-                if event.direction == 'up':
-                    ana = 1
-                elif event.direction == 'down':
-                    ana = 2
-                elif event.direction == 'left':
-                    ana = 3
-                elif event.direction == 'right':
-                    ana = 4
+            
+            #Checking for movment in the joystick
+            while ana != anum:
+                for event in sense.stick.get_events():
+               # Check if joystick is pressed
+                    if event.action == 'pressed':
                     
-                    
-                    
-    
-    
-  #Make function of calling the arrow, then just
-  # Calling time 
-  
-    sense.clear()
-    time.sleep(pause)
-    clock = round(time.time()-start_time,2)
-    print(clock)
+                    # Check which direcion has been pressed
+                        if event.direction == 'up':
+                            ana = 1
+                        elif event.direction == 'right':
+                            ana = 2
+                        elif event.direction == 'down':
+                            ana = 3
+                        elif event.direction == 'left':
+                            ana = 4
+                            
+                            
+                            
+            
+            
+          #Make function of calling the arrow, then just
+          # Calling time 
+          
+            sense.clear()
+            
+            clock = (round(time.time()-start_time,2))
+            time.sleep(pause)
+            trys = trys + 1
+            print(clock)
+            
+            average = (average + clock)
+            
 
-# adding the orientation feature 
+        # adding the orientation feature 
 
-    
-    
+            
+            
+
+        #Check to see if the code works correctly up until this point 
+        sense.show_letter("M")
 
 
-sense.show_letter("M")
-
-
-        
-a = sense.get_accelerometer_raw()
-x = a["x"]
-y = a["y"]
-z = a["z"]
-        
-x = int(round(x,0))
-y = int(round(y,0))
-z = int(round(z,0))
-        
-print(f"X:{x} Y:{y} Z:{z}")
-        
-if x == 1 :
-    sense.set_rotation(270)
-elif x == -1:
-    sense.set_rotation(90)
-elif y == -1:
-    sense.set_rotation(180)
-else:
-    sense.set_rotation(0)
-
-sense.show_message("Your current orientation is",f"X:{x} Y:{y} Z:{z}", text_colour=R, back_colour=N, scroll_speed=0.1)
+                
+        a = sense.get_accelerometer_raw()
+        x = a["x"]
+        y = a["y"]
+        z = a["z"]
+                #Rounding orientation variables in sense.show_message
+                #Printing final message 
+        sense.show_message("Your:" f" AVG:{(average)/10} X:{round(x, 2)} Y:{round(y, 2)} Z:{round(z, 2)}")
+        #Checking that the code runs
+        print('finished')
+        #Ending the loop
+        run = 0        
